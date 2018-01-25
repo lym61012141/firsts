c3feb1b3ab2f1c9a78c4a8f1b74a31db2a1305ea
Liuym <liuym@startimes.com.cn>
2018-01-25 18:45:40 +0800

    add annotation

--- src/main/java/com/minger/firsts/annotation/RandomId.java
+++ src/main/java/com/minger/firsts/annotation/RandomId.java
@@ -1,12 +1,15 @@
 package com.minger.firsts.annotation;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
  * Create By Liuym on 2018/1/25 0025
  */
 @Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface RandomId {
 
 }
